6cc6ce97923f3b53d94d40fba83024c2d4bb7192
YasinEled <yasinelede@gmail.com>
2025-01-27 19:49:39 +0100

    pagina iniciar session

--- index.php
+++ index.php
@@ -12,18 +12,34 @@
     <div class="marginHeader"></div>
     <div class="container">
         <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center">
-            <div class= "col-12 col-lg-6 mr-5">
-                <img src="./img/logoGrande.png" class="logoMini" alt="Logo">
+            <div class= "col-12 col-lg-6 mr-4">
+                <img src="./ASSETS/CampusJob.png" class="logoMini" alt="Logo">
             </div>
-            <div class= "col-12 col-lg-6 ml-5">
-                <h1 class="titulo">Tu voz <br> Tu espacio <br> Tu anonimato</h1>
-                <h2>Únete hoy</h2>
-                <a href="#registerModal" class="btn-crear-cuenta">Crear cuenta</a>
-                <p class="p-login">¿ Tienes una cuenta ?</p>
-                <a href="#loginModal" class="btn-login">Iniciar Sesión</a>
+            <div class= "col-12 col-lg-6 ml-4">
+                <form action="./PHP/login.php" method="POST">
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Username">
+                        <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn-login">Iniciar Sesión</button>
+                </form>
+                <p class="p-login">¿ Eres un nuevo centro estudiantil ?</p>
+                <a href="#loginModal" class="btn-crear-cuenta">Contactanos</a>
             </div>
         </div>
     </div>
-
+    <footer class="col-12 text-center">
+        <div class = "footer-links">
+            <a href="#">Informació</a>
+            <a href="#">Ayuda</a>
+            <a href="#">política de cookies</a>
+            <a href="#">Política de privacidad</a>
+            <a href="#">Condiciones de servicio</a>
+            <a href="#">Accesibilitat</a>
+        </div>
+        <div>
+            <p class="p-footer">© 2025 Anonims Corp.</p>
+        </div>
+    </footer>
 </body>
 </html>
