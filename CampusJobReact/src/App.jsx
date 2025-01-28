722e449b29fc6222b33663c58bd8a54c000e4158
YasinEled <yasinelede@gmail.com>
2025-01-28 19:42:21 +0100

    update main

--- CampusJobReact/src/App.jsx
+++ CampusJobReact/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div class="marginHeader"></div>
+      <div class="container">
+        <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center">
+          <div class= "col-12 col-lg-6 mr-5 ml-5">
+            <img class="logoMini" src="/src/assets/Logo/CampusJob.png" alt="Logo" />
+          </div>
+            <div class= "col-12 col-lg-6 ml-5 mr-5">
+              <form action="./PHP/login.php" method="POST">
+                <div class="form-group">
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Username"/>
+                  <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password"/>
+                </div>
+                <button type="submit" class="btn-login">Iniciar Sesión</button>
+              </form>
+              <p class="p-login">¿ Eres un nuevo centro estudiantil ?</p>
+              <a href="#loginModal" class="btn-crear-cuenta">Contactanos</a>
+            </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div class="marginFooter"></div>
+    <footer class="col-12 text-center">
+        <div class = "footer-links">
+            <a href="#">Informació</a>
+            <a href="#">Ayuda</a>
+            <a href="#">política de cookies</a>
+            <a href="#">Política de privacidad</a>
+            <a href="#">Accesibilitat</a>
+            <a href="#">Condiciones de servicio</a>
+        </div>
+        <div>
+            <p class="p-footer">© 2025 Campus Job.</p>
+        </div>
+    </footer>
+    </main>
   )
 }
 
