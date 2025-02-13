284dba1c9ff2e5f3b347f92f41c167744f7bf57a
YasinEled <yasinelede@gmail.com>
2025-02-13 19:52:03 +0100

    arreglando responsivs

--- CampusJobReact/src/Components/Header-bg.jsx
+++ CampusJobReact/src/Components/Header-bg.jsx
@@ -2,12 +2,12 @@ import "./ComponentsCSS/headerBg.css";
 
 function HeaderBg() {
     return (
-        <div className="absolute  fondo  items-center justify-center">
-            <div className="absolute top-100 left-20 w-166 h-90  rounded-full bola-Rosa"></div>
+        <div className="absolute   w-full py-10 h-full  overflow-hidden items-center justify-center">
+            <div className="absolute left-20   rounded-full bola-Rosa1"></div>
 
-            <div className="absolute topp right-15  h-130 w-200 rounded-full bola-Rosa"></div>
-            <div className="absolute topp right-150 h-145 w-200 rounded-full bola-Amarilla"></div>
-            <div className="absolute topp left-20 w-166 h-90 rounded-full bola-Azul"></div>
+            <div className="absolute  right-15  rounded-full bola-Rosa"></div>
+            <div className="absolute  right-150  rounded-full bola-Amarilla"></div>
+            <div className="absolute  left-20  rounded-full bola-Azul"></div>
 
             <div className="absolute inset-0 borosso"></div>
         </div>
