622d6f97f8dc65a1966bd780b945abf7dc3e5339
Alexrupa27 <alexrupa123@gmail.com>
2025-04-03 18:35:16 +0200

    not found fixed

--- CampusJobReact/src/NotFound.jsx
+++ CampusJobReact/src/NotFound.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import fondo404 from './assets/img/Fondo404.png'
+import fondo404 from './assets/img/Fondo404.png';
+import '../src/Components/ComponentsCSS/NotFound.css';
+
+import Menu from "./Components/MenuHome/Menu/menu";
+import { Outlet } from "react-router-dom";  // Importa Outlet
+
+import NavHome from "./Components/MenuHome/Nav/NavHome";
+import './Components/ComponentsCSS/MenuHome/general.css';
 
 function NotFound() {
   return (
-    <div>
-      <img className="imagen404" src={fondo404} alt="" />
-      <h1>Error 404</h1>
-      <p>Página no encontrada.</p>
+    <div className="NotFoundWrapper">
+      <NavHome />
+      <Menu /> 
+      <Outlet />
+      <div className="container404">
+        <div className="Texto404">
+          <p className="TituloError404">404</p>
+          <p className="SubtituloError404">OOPS!</p>
+          <p className="ExplicacionError404">Parece que te perdiste. Esta página no existe o fue movida.  📭 🚀</p>
+        </div>
+      </div>
     </div>
   );
 }
