1c56b7f6f98003634aa7ec74e256cd3773dbd383
Alexrupa27 <alexrupa123@gmail.com>
2025-05-08 20:16:17 +0200

    fix

--- CampusJobReact/src/Components/MenuProfesor/AñadirCursos.jsx
+++ CampusJobReact/src/Components/MenuProfesor/AñadirCursos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AñadirCursos() {
   return (
-    <div className="p-6 space-y-4 max-w-md mx-auto">
+    <div className="AñadirCursosContenedor">
 
     </div>
   );
