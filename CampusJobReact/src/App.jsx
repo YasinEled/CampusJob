963170e0e5b939de2bde2db738bc8b6063f39424
oriolpujadas <oriol.pujadasmontane@gmail.com>
2025-05-14 18:46:22 +0200

    ofertas mostrar

--- CampusJobReact/src/App.jsx
+++ CampusJobReact/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     event.preventDefault();
 
     try {
-        const response = await fetch('https://campusjobbackend.onrender.com/api/auth/login', {
+        const response = await fetch('http://localhost:4000/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
